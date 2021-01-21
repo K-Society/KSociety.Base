ed49d2ee5599e9ba3eb6a75cae322b40b63575bc
maniglia <a.klede@hotmail.it>
2021-01-21 10:40:19 +0100

    Add IAgentImportModel.

---
+++ Src/01/02/KSociety.Base.Srv.Agent/IAgentExportModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace KSociety.Base.Srv.Agent
+{
+    public interface IExportModel<in TExportReq, TExportRes>
+        where TExportReq : class
+        where TExportRes : class
+    {
+        TExportRes Export(TExportReq exportReq, CancellationToken cancellationToken = default);
+
+        ValueTask<TExportRes> ExportAsync(TExportReq exportReq, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Src/01/02/KSociety.Base.Srv.Agent/IAgentImportModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace KSociety.Base.Srv.Agent
+{
+    public interface IImportModel<in TImportReq, TImportRes>
+        where TImportReq : class
+        where TImportRes : class
+    {
+        TImportRes Import(TImportReq importReq, CancellationToken cancellationToken = default);
+
+        ValueTask<TImportRes> ImportAsync(TImportReq importReq, CancellationToken cancellationToken = default);
+    }
+}
