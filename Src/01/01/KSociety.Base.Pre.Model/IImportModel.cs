1f8e7203e7fae9e8a08386ba59fa43fb3185529f
maniglia <a.klede@hotmail.it>
2021-01-21 12:24:19 +0100

    KSociety.Base.Pre.Model fix.

--- Src/01/01/KSociety.Base.Pre.Model/IImportModel.cs
+++ Src/01/01/KSociety.Base.Pre.Model/IImportModel.cs
@@ -1,14 +1,14 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
+﻿//using System.Threading;
+//using System.Threading.Tasks;
 
-namespace KSociety.Base.Pre.Model
-{
-    public interface IImportModel<in TImportReq, TImportRes>
-        where TImportReq : class
-        where TImportRes : class
-    {
-        TImportRes Import(TImportReq importReq, CancellationToken cancellationToken = default);
+//namespace KSociety.Base.Pre.Model
+//{
+//    public interface IImportModel<in TImportReq, TImportRes>
+//        where TImportReq : class
+//        where TImportRes : class
+//    {
+//        TImportRes Import(TImportReq importReq, CancellationToken cancellationToken = default);
 
-        ValueTask<TImportRes> ImportAsync(TImportReq importReq, CancellationToken cancellationToken = default);
-    }
-}
+//        ValueTask<TImportRes> ImportAsync(TImportReq importReq, CancellationToken cancellationToken = default);
+//    }
+//}
---
+++ Src/01/02/KSociety.Base.Srv.Agent/List/IAgentQueryModel.cs
@@ -0,0 +1,15 @@
+﻿using KSociety.Base.InfraSub.Shared.Interface;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KSociety.Base.Srv.Agent.List
+{
+    public interface IAgentQueryModel<T, TList>
+        where T : IObject
+        where TList : InfraSub.Shared.Interface.IList<T>
+    {
+        TList LoadAllRecords(CancellationToken cancellationToken = default);
+
+        ValueTask<TList> LoadAllRecordsAsync(CancellationToken cancellationToken = default);
+    }
+}
