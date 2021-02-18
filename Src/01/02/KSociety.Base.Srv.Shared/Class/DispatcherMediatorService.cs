dddc6dae6d4387eae8cffe2ed1e79bc292d230ac
maniglia <a.klede@hotmail.it>
2021-02-18 12:16:27 +0100

    Modified Mediatr Autofac.Module.

---
+++ Src/01/02/KSociety.Base.Srv.Shared/Class/DispatcherMediatorService.cs
@@ -0,0 +1,30 @@
+﻿//using MediatR;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace KSociety.Base.Srv.Shared.Class
+//{
+//    public class DispatcherMediatorService
+//    {
+//        private readonly IMediator _mediator;
+
+//        public DispatcherMediatorService(IMediator mediator)
+//        {
+//            _mediator = mediator;
+//        }
+
+//        public void Dispatch(INotification notifyMessage)
+//        {
+//            _mediator.Send(notifyMessage);
+//        }
+
+//        public async ValueTask DispatchAsync(INotification notifyMessage, CancellationToken cancellationToken = default)
+//        {
+//            await _mediator.Send(notifyMessage, cancellationToken).ConfigureAwait(false);
+//        }
+//    }
+//}
