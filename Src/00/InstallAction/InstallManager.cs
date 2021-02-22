fec7c37ea2943d264775ac91c4cdd4c9d6ecb0c6
maniglia <a.klede@hotmail.it>
2021-02-22 09:10:05 +0100

    Added InstallAction.

---
+++ Src/00/InstallAction/InstallManager.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using System.Diagnostics;
+using WixSharp;
+
+namespace InstallAction
+{
+    public static class InstallManager
+    {
+        [CustomAction]
+        public static ActionResult SilentInstall(Session session)
+        {
+            return session.HandleErrors(() =>
+            {
+                var data = session.CustomActionData;
+                var exeName = data["ExeName"];
+                var args = data["Args"];
+
+                var process = new Process
+                {
+                    StartInfo =
+                    {
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        FileName = exeName,
+                        Arguments = args
+                    }
+                };
+                process.Start();
+                process.WaitForExit();
+            });
+        }
+
+        [CustomAction]
+        public static ActionResult SilentUninstall(Session session)
+        {
+            return session.HandleErrors(() =>
+            {
+                var data = session.CustomActionData;
+                var exeName = data["ExeName"];
+                var args = data["Args"];
+
+                var process = new Process()
+                {
+                    StartInfo =
+                    {
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        FileName = exeName,
+                        Arguments = args
+                    }
+                };
+                process.Start();
+                process.WaitForExit();
+            });
+        }
+    }
+}
---
+++ Src/00/InstallAction/ServiceManager.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using WixSharp;
+
+namespace InstallAction
+{
+    public static class ServiceManager
+    {
+        [CustomAction]
+        public static ActionResult InstallService(Session session)
+        {
+            return session.HandleErrors(() =>
+            {
+                var data = session.CustomActionData;
+                string installState = data["InstallState"];
+                string serviceName = data["ServiceName"];
+                installState = installState.Remove(installState.Length - 1);
+
+                string args = "/i /LogFile= /LogToConsole=false /InstallStateDir=" + "\"" + installState + "\"";
+
+                System.Diagnostics.Process process = System.Diagnostics.Process.Start(serviceName, args);
+                process?.WaitForExit(1000 * 60 * 5);// Wait up to five minutes.
+
+            });
+        }
+
+        [CustomAction]
+        public static ActionResult UnInstallService(Session session)
+        {
+            return session.HandleErrors(() =>
+            {
+                var data = session.CustomActionData;
+                string installState = data["InstallState"];
+                string serviceName = data["ServiceName"];
+                installState = installState.Remove(installState.Length - 1);
+
+                string args = "/u /LogFile= /LogToConsole=false /InstallStateDir=" + "\"" + installState + "\"";
+
+                System.Diagnostics.Process process = System.Diagnostics.Process.Start(serviceName, args);
+                process?.WaitForExit(1000 * 60 * 5);// Wait up to five minutes.
+            });
+        }
+    }
+}
