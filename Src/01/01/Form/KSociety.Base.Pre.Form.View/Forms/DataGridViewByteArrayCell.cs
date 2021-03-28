58e0ce28d36412cd04ffe6c1ab2349c88c8d200a
maniglia <a.klede@hotmail.it>
2021-03-28 16:55:51 +0200

    Added Presenter and View.

---
+++ Src/01/01/Form/KSociety.Base.Pre.Form.View/Forms/DataGridViewByteArrayCell.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Forms;
+
+namespace KSociety.Base.Pre.Form.View.Forms
+{
+    public class DataGridViewByteArrayCell : DataGridViewTextBoxCell
+    {
+    }
+}
---
+++ Src/01/01/Form/KSociety.Base.Pre.Form.View/Forms/DataGridViewComboBoxColumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSociety.Base.Pre.Form.View.Forms
+{
+    public partial class DataGridViewComboBoxColumn : System.Windows.Forms.DataGridViewComboBoxColumn
+    {
+        public DataGridViewComboBoxColumn()
+        {
+            InitializeComponent();
+        }
+
+        public DataGridViewComboBoxColumn(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
