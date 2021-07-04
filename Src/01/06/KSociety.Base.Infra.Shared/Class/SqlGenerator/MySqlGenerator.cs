bc5c0853e1368a8d62f808154c9d524a4b2de3b3
maniglia <a.klede@hotmail.it>
2021-07-04 10:49:20 +0200

    Added SqlGeneratorHelper class.

--- Src/01/06/KSociety.Base.Infra.Shared/Class/SqlGenerator/MySqlGenerator.cs
+++ Src/01/06/KSociety.Base.Infra.Shared/Class/SqlGenerator/MySqlGenerator.cs
@@ -1,13 +1,10 @@
-﻿using KSociety.Base.InfraSub.Shared.Class;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.Extensions.Logging;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure.Internal;
 using Pomelo.EntityFrameworkCore.MySql.Migrations;
 using System;
-using System.IO;
-using System.Linq;
 
 namespace KSociety.Base.Infra.Shared.Class.SqlGenerator
 {
@@ -36,41 +33,13 @@ namespace KSociety.Base.Infra.Shared.Class.SqlGenerator
             {
                 if (operation is CreateViewOperation createViewOperation)
                 {
-                    Generate(createViewOperation, builder);
+                    //Generate(createViewOperation, builder);
+                    SqlGeneratorHelper.Generate(_logger, createViewOperation, builder, Dependencies.SqlGenerationHelper);
                 }
                 else
                 {
                     base.Generate(operation, model, builder);
                 }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError("Generate " + ex.Message + " " + ex.StackTrace);
-            }
-        }
-
-        private void Generate(
-            CreateViewOperation operation,
-            MigrationCommandListBuilder builder)
-        {
-            try
-            {
-                var sqlHelper = Dependencies.SqlGenerationHelper;
-
-                var assembly = AssemblyTool.GetAssemblyByName(operation.AssemblyName);
-
-                string resourceName = assembly.GetManifestResourceNames()
-                    .Single(str => str.EndsWith(operation.ResourceSqlFileName));
-
-                using Stream stream = assembly.GetManifestResourceStream(resourceName);
-                using StreamReader reader = new(stream ?? throw new InvalidOperationException());
-                string result = reader.ReadToEnd();
-
-                _logger.LogDebug(result);
-
-                builder.AppendLines(result)
-                    .AppendLine(sqlHelper.StatementTerminator)
-                    .EndCommand();
             }
             catch (Exception ex)
             {
