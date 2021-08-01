66cc9b53ffa8dba2f77c68f208acb7e9621a1ada
maniglia <a.klede@hotmail.it>
2021-08-01 14:16:51 +0200

    Added ContextFactory method.

---
+++ Src/01/06/KSociety.Base.Infra.Shared/Class/ContextFactory.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace KSociety.Base.Infra.Shared.Class
+{
+    public class ContextFactory<TContext> : IDesignTimeDbContextFactory<TContext> 
+        where TContext : DatabaseContext
+    {
+        public TContext CreateDbContext(string[] args)
+        {
+            TContext output = null;
+            var dbEngine = DatabaseEngine.Sqlserver;
+            var migrationsAssembly = string.Empty;
+            var connectionString = string.Empty;
+
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "Sqlserver":
+                        dbEngine = DatabaseEngine.Sqlserver;
+                        break;
+                    case "Sqlite":
+                        dbEngine = DatabaseEngine.Sqlite;
+                        break;
+                    case "Npgsql":
+                        dbEngine = DatabaseEngine.Npgsql;
+                        break;
+                    case "MySql":
+                        dbEngine = DatabaseEngine.Mysql;
+                        break;
+                }
+            }
+
+            if (args.Length >= 2)
+            {
+                connectionString = args[1];
+            }
+
+            if (args.Length >= 3)
+            {
+                migrationsAssembly = args[2];
+            }
+
+            var optionBuilder = new DbContextOptionsBuilder<TContext>();
+
+            switch (dbEngine)
+            {
+                case DatabaseEngine.Sqlserver:
+                    optionBuilder
+                    .UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    break;
+
+                case DatabaseEngine.Sqlite:
+                    optionBuilder
+                        .UseSqlite(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    break;
+
+                case DatabaseEngine.Npgsql:
+                    break;
+
+                case DatabaseEngine.Mysql:
+                    optionBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), sql => sql.MigrationsAssembly(migrationsAssembly));
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            try
+            {
+                output = (TContext)Activator.CreateInstance(typeof(TContext), optionBuilder.Options);
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            return output;
+        }
+    }
+}
