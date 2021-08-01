0d1c5944e92150d69ccdc0f59afc55993963eb73
maniglia <a.klede@hotmail.it>
2021-08-01 16:06:10 +0200

    DesignTimeDbContextFactory test.

--- Src/01/06/KSociety.Base.Infra.Shared/Class/ContextFactory.cs
+++ Src/01/06/KSociety.Base.Infra.Shared/Class/ContextFactory.cs
@@ -1,98 +1,98 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using System;
+﻿//using KSociety.Base.Infra.Shared.Interface;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
+//using System;
 
-namespace KSociety.Base.Infra.Shared.Class
-{
-    public class ContextFactory<TContext> : IDesignTimeDbContextFactory<TContext> 
-        where TContext : DatabaseContext
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public virtual TContext CreateDbContext(string[] args)
-        {
-            TContext output = null;
-            var dbEngine = DatabaseEngine.Sqlserver;
-            var migrationsAssembly = string.Empty;
-            var connectionString = string.Empty;
+//namespace KSociety.Base.Infra.Shared.Class
+//{
+//    public class ContextFactory : IContextFactory<TContext> where TContext : DatabaseContext
+//    {
+//        /// <summary>
+//        /// 
+//        /// </summary>
+//        /// <param name="args"></param>
+//        /// <returns></returns>
+//        public virtual TContext CreateDbContext(string[] args)
+//        {
+//            TContext output = null;
+//            var dbEngine = DatabaseEngine.Sqlserver;
+//            var migrationsAssembly = string.Empty;
+//            var connectionString = string.Empty;
 
-            if (args.Length < 3) return output;
+//            if (args.Length < 2) return output;
 
-            if (args.Length > 0)
-            {
-                switch (args[0])
-                {
-                    case "Sqlserver":
-                        dbEngine = DatabaseEngine.Sqlserver;
-                        break;
-                    case "Sqlite":
-                        dbEngine = DatabaseEngine.Sqlite;
-                        break;
-                    case "Npgsql":
-                        dbEngine = DatabaseEngine.Npgsql;
-                        break;
-                    case "MySql":
-                        dbEngine = DatabaseEngine.Mysql;
-                        break;
-                }
-            }
+//            if (args.Length > 0)
+//            {
+//                switch (args[0])
+//                {
+//                    case "Sqlserver":
+//                        dbEngine = DatabaseEngine.Sqlserver;
+//                        break;
+//                    case "Sqlite":
+//                        dbEngine = DatabaseEngine.Sqlite;
+//                        break;
+//                    case "Npgsql":
+//                        dbEngine = DatabaseEngine.Npgsql;
+//                        break;
+//                    case "MySql":
+//                        dbEngine = DatabaseEngine.Mysql;
+//                        break;
+//                }
+//            }
 
-            if (args.Length >= 2)
-            {
-                Console.WriteLine("string: " 
-                    + args[1]);
-                connectionString = args[1];
-            }
+//            //if (args.Length >= 2)
+//            //{
+//            //    Console.WriteLine("string: " 
+//            //        + args[1]);
+//            //    connectionString = args[1];
+//            //}
 
-            if (args.Length >= 3)
-            {
-                migrationsAssembly = args[2];
-            }
+//            if (args.Length >= 2)
+//            {
+//                migrationsAssembly = args[1];
+//            }
 
-            var optionBuilder = new DbContextOptionsBuilder<TContext>();
+//            var optionBuilder = new DbContextOptionsBuilder<TContext>();
 
-            switch (dbEngine)
-            {
-                case DatabaseEngine.Sqlserver:
-                    optionBuilder
-                        .EnableDetailedErrors()
-                        .EnableSensitiveDataLogging()
-                        .UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+//            switch (dbEngine)
+//            {
+//                case DatabaseEngine.Sqlserver:
+//                    optionBuilder
+//                        .EnableDetailedErrors()
+//                        .EnableSensitiveDataLogging()
+//                        .UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
 
-                    break;
+//                    break;
 
-                case DatabaseEngine.Sqlite:
-                    optionBuilder
-                        .UseSqlite(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+//                case DatabaseEngine.Sqlite:
+//                    optionBuilder
+//                        .UseSqlite(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
 
-                    break;
+//                    break;
 
-                case DatabaseEngine.Npgsql:
-                    break;
+//                case DatabaseEngine.Npgsql:
+//                    break;
 
-                case DatabaseEngine.Mysql:
-                    optionBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), sql => sql.MigrationsAssembly(migrationsAssembly));
-                    break;
+//                case DatabaseEngine.Mysql:
+//                    optionBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), sql => sql.MigrationsAssembly(migrationsAssembly));
+//                    break;
 
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+//                default:
+//                    throw new ArgumentOutOfRangeException();
+//            }
 
-            //try
-            //{
+//            //try
+//            //{
                 
-            //}
-            //catch(Exception ex)
-            //{
+//            //}
+//            //catch(Exception ex)
+//            //{
 
-            //}
+//            //}
 
-            output = (TContext)Activator.CreateInstance(typeof(TContext), optionBuilder.Options);
+//            output = (TContext)Activator.CreateInstance(typeof(TContext), optionBuilder.Options);
 
-            return output;
-        }
-    }
-}
+//            return output;
+//        }
+//    }
+//}
