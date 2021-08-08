d304859b37d9c102608f1d520bc6be94d57b7a50
maniglia <a.klede@hotmail.it>
2021-08-08 22:10:13 +0200

    DatabaseConfiguration LazyLoading.

--- Src/01/02/Host/KSociety.Base.Srv.Host.Shared/Bindings/DatabaseConfiguration.cs
+++ Src/01/02/Host/KSociety.Base.Srv.Host.Shared/Bindings/DatabaseConfiguration.cs
@@ -12,19 +12,21 @@ namespace KSociety.Base.Srv.Host.Shared.Bindings
         private readonly string _masterString;
         private readonly bool _debugFlag;
         private readonly string _migrationsAssembly;
+        private readonly bool _lazyLoading;
 
         public DatabaseConfiguration(DatabaseEngine databaseEngine, 
-            string masterString, bool debugFlag, string migrationsAssembly = "")
+            string masterString, bool debugFlag, string migrationsAssembly = "", bool lazyLoading = false)
         {
             _databaseEngine = databaseEngine;
             _masterString = masterString;
             _debugFlag = debugFlag;
             _migrationsAssembly = migrationsAssembly;
+            _lazyLoading = lazyLoading;
         }
 
         protected override void Load(ContainerBuilder builder)
         {
-            var conf = new Infra.Shared.Class.DatabaseConfiguration(_databaseEngine, _masterString, _debugFlag, _migrationsAssembly);
+            var conf = new Infra.Shared.Class.DatabaseConfiguration(_databaseEngine, _masterString, _debugFlag, _migrationsAssembly, _lazyLoading);
             builder.RegisterInstance(conf).AsSelf().SingleInstance();
         }
     }
