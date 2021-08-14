87ae2886d7a1e3789bff08dde00314508d7db529
maniglia <a.klede@hotmail.it>
2021-08-14 16:01:51 +0200

    Added ContextFactory.

---
+++ Src/01/06/KSociety.Base.Infra.Shared/Interface/IContextFactory.cs
@@ -0,0 +1,9 @@
+﻿using KSociety.Base.Infra.Shared.Class;
+
+namespace KSociety.Base.Infra.Shared.Interface
+{
+    public interface IContextFactory<out TContext> where TContext : DatabaseContext
+    {
+        TContext CreateDbContext(string[] args);
+    }
+}
