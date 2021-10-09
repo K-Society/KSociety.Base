173ac5d1f7c5b85298f98739420d71c33e41ee8e
maniglia <kSociety85@gmail.com>
2021-10-09 10:21:33 +0200

    MessageBroker module for Autofac updated.

--- Src/01/02/Host/KSociety.Base.Srv.Host.Shared/Class/MessageBroker.cs
+++ Src/01/02/Host/KSociety.Base.Srv.Host.Shared/Class/MessageBroker.cs
@@ -6,26 +6,4 @@
         public ExchangeDeclareParameters ExchangeDeclareParameters { get; set; }
         public QueueDeclareParameters QueueDeclareParameters { get; set; }
     }
-
-    public class ConnectionFactory
-    {
-        public string MqHostName { get; set; } = "localhost";
-        public string MqUserName { get; set; } = "KSociety";
-        public string MqPassword { get; set; } = "KSociety";
-    }
-
-    public class ExchangeDeclareParameters
-    {
-        public string BrokerName { get; set; }
-        public Base.EventBus.ExchangeType ExchangeType { get; set; } = EventBus.ExchangeType.Direct;
-        public bool ExchangeDurable { get; set; } = false;
-        public bool ExchangeAutoDelete { get; set; } = true;
-    }
-
-    public class QueueDeclareParameters
-    {
-        public bool QueueDurable { get; set; } = false;
-        public bool QueueExclusive { get; set; } = false;
-        public bool QueueAutoDelete { get; set; } = true;
-    }
 }
