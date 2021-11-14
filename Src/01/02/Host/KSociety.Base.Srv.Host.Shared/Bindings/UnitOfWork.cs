9b0096d0859c74bc289946e58dae51373ead69b2
maniglia <kSociety85@gmail.com>
2021-11-14 17:15:38 +0100

    csprojs style fix.

--- Src/01/02/Host/KSociety.Base.Srv.Host.Shared/Bindings/UnitOfWork.cs
+++ Src/01/02/Host/KSociety.Base.Srv.Host.Shared/Bindings/UnitOfWork.cs
@@ -5,9 +5,9 @@ using KSociety.Base.Infra.Shared.Interface;
 namespace KSociety.Base.Srv.Host.Shared.Bindings
 {
     /// <summary>
-    /// 
+    /// The UnitOfWork module for Autofac.
     /// </summary>
-    /// <typeparam name="TContext"></typeparam>
+    /// <typeparam name="TContext">TContext is DatabaseContext</typeparam>
     public class UnitOfWork<TContext> : Module where TContext : DatabaseContext
     {
         protected override void Load(ContainerBuilder builder)
