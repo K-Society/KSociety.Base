6c1e1c13838300032625c2a560a3bbc9d687ce6e
maniglia <kSociety85@gmail.com>
2021-12-22 10:51:32 +0100

    Added Logger to ContextFactory.cs

--- Src/01/06/KSociety.Base.Infra.Shared/Class/ContextFactory.cs
+++ Src/01/06/KSociety.Base.Infra.Shared/Class/ContextFactory.cs
@@ -3,18 +3,37 @@ using KSociety.Base.Infra.Shared.Interface;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
 
 namespace KSociety.Base.Infra.Shared.Class;
 
 public class ContextFactory<TContext> : IContextFactory<TContext> where TContext : DatabaseContext
 {
+    private readonly ILogger<ContextFactory<TContext>> _logger;
+
+    public ContextFactory()
+    {
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+            builder.SetMinimumLevel(LogLevel.Trace);
+        });
+        _logger = loggerFactory.CreateLogger<ContextFactory<TContext>>();
+    }
+
+    public ContextFactory(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<ContextFactory<TContext>>();
+    }
+
     public virtual TContext CreateDbContext(string[] args)
     {
+        _logger.LogTrace("ContextFactory CreateDbContext");
         TContext output = null;
         var dbEngine = DatabaseEngine.Sqlserver;
         var migrationsAssembly = string.Empty;
-        var connectionStringName = string.Empty;
         var connectionString = string.Empty;
 
         if (args.Length < 2) return output;
@@ -22,7 +41,6 @@ public class ContextFactory<TContext> : IContextFactory<TContext> where TContext
         if (args.Length > 0)
         {
             switch (args[0])
-                //switch (databaseEngine)
             {
                 case "Sqlserver":
                     dbEngine = DatabaseEngine.Sqlserver;
@@ -41,17 +59,21 @@ public class ContextFactory<TContext> : IContextFactory<TContext> where TContext
 
         if (args.Length >= 2)
         {
-            connectionStringName = args[1];
+            string connectionStringName = args[1];
 
             var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
                 .Build();
             connectionString = config.GetConnectionString(connectionStringName);
+            _logger.LogTrace(@"ContextFactory CreateDbContext connectionString: {0}", connectionString);
         }
 
         if (args.Length >= 3)
         {
             migrationsAssembly = args[2];
+
+            _logger.LogTrace(@"ContextFactory CreateDbContext migrationsAssembly: {0}", migrationsAssembly);
         }
 
         var optionBuilder = new DbContextOptionsBuilder<TContext>();
