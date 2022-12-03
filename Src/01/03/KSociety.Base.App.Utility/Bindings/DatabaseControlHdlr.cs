48a442e2cb33ddd09a0c44a501ef5699a3b37641
maniglia <kSociety85@gmail.com>
2022-12-04 00:23:29 +0100

    Code reorganization. *Bindings

--- Src/01/03/KSociety.Base.App.Utility/Bindings/DatabaseControlHdlr.cs
+++ Src/01/03/KSociety.Base.App.Utility/Bindings/DatabaseControlHdlr.cs
@@ -2,18 +2,13 @@
 using KSociety.Base.App.Shared;
 using KSociety.Base.App.Utility.Dto.Res.Control;
 using KSociety.Base.App.Utility.ReqHdlr;
-using KSociety.Base.Infra.Abstraction.Interface;
-using KSociety.Base.Infra.Shared.Class;
-using KSociety.Base.Infra.Shared.Interface;
-using Microsoft.Extensions.Logging;
 
-namespace KSociety.Base.Srv.Host.Shared.Bindings
+namespace KSociety.Base.App.Utility.Bindings
 {
     /// <summary>
     /// 
     /// </summary>
-    /// <typeparam name="TContext"></typeparam>
-    public class DatabaseControl<TContext> : Module where TContext : DatabaseContext
+    public class DatabaseControlHdlr : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
@@ -33,8 +28,8 @@ namespace KSociety.Base.Srv.Host.Shared.Bindings
                 .Named<IRequestHandler>("MigrationReqHdlr")
                 .Named<IRequestHandlerAsync>("MigrationReqHdlr");
 
-            builder.RegisterType<DatabaseFactory<ILoggerFactory, IDatabaseConfiguration, TContext>>()
-                .As<IDatabaseFactory<TContext>>().InstancePerLifetimeScope();
+            //builder.RegisterType<DatabaseFactory<ILoggerFactory, IDatabaseConfiguration, TContext>>()
+            //    .As<IDatabaseFactory<TContext>>().InstancePerLifetimeScope();
         }
     }
 }
---
+++ Src/01/03/KSociety.Base.App.Utility/Bindings/Identity/DatabaseControlHdlr.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using KSociety.Base.App.Shared;
+using KSociety.Base.App.Utility.Dto.Res.Control;
+using KSociety.Base.App.Utility.ReqHdlr;
+
+namespace KSociety.Base.App.Utility.Bindings.Identity
+{
+    public class DatabaseControlHdlr : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<GetConnectionStringReqHdlr>()
+                .As(typeof(IRequestHandlerWithResponse<ConnectionString>));
+            builder.RegisterType<EnsureCreatedReqHdlr>().As(typeof(IRequestHandlerWithResponse<EnsureCreated>)).As(
+                typeof(IRequestHandlerWithResponseAsync<EnsureCreated>));
+            builder.RegisterType<EnsureDeletedReqHdlr>().As(typeof(IRequestHandlerWithResponse<EnsureDeleted>)).As(
+                typeof(IRequestHandlerWithResponseAsync<EnsureDeleted>));
+            builder.RegisterType<MigrationReqHdlr>().Named<IRequestHandler>("MigrationReqHdlr")
+                .Named<IRequestHandlerAsync>("MigrationReqHdlr");
+
+            //builder
+            //    .RegisterType<DatabaseFactory<ILoggerFactory, IDatabaseConfiguration, TContext, TUser, TRole, TKey,
+            //        TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken>>()
+            //    .As<IDatabaseFactory<TContext, TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim,
+            //        TUserToken>>().InstancePerLifetimeScope();
+        }
+    }
+}
