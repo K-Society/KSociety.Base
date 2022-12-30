6d25860c9d7e8ff2ed2fef9baebac75e4778053d
maniglia <kSociety85@gmail.com>
2022-12-30 20:42:49 +0100

    Fix in KSociety.Base.Srv.Agent.

---
+++ Src/01/02/KSociety.Base.Srv.Agent/ListKeyValue/IAgentQueryKeyValueModelAsync.cs
@@ -0,0 +1,12 @@
+﻿using KSociety.Base.Srv.Dto;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KSociety.Base.Srv.Agent.ListKeyValue
+{
+    public interface IAgentQueryKeyValueModelAsync<TList, TKey, TValue>
+        where TList : ListKeyValuePair<TKey, TValue>
+    {
+        ValueTask<TList> LoadDataAsync(CancellationToken cancellationToken = default);
+    }
+}
