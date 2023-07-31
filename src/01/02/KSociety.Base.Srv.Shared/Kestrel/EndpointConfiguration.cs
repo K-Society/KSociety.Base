dc882c1c5486f4710597c8566e456d3d2eb8311a
maniglia <kSociety85@gmail.com>
2023-07-31 08:17:39 +0200

    Removed Kastrel.

--- src/01/02/KSociety.Base.Srv.Shared/Kestrel/EndpointConfiguration.cs
+++ src/01/02/KSociety.Base.Srv.Shared/Kestrel/EndpointConfiguration.cs
@@ -1,13 +1,13 @@
-﻿namespace KSociety.Base.Srv.Shared.Kestrel
-{
-    public class EndpointConfiguration
-    {
-        public string Host { get; set; }
-        public int? Port { get; set; }
-        public string Scheme { get; set; }
-        public string StoreName { get; set; }
-        public string StoreLocation { get; set; }
-        public string FilePath { get; set; }
-        public string Password { get; set; }
-    }
-}
+﻿//namespace KSociety.Base.Srv.Shared.Kestrel
+//{
+//    public class EndpointConfiguration
+//    {
+//        public string Host { get; set; }
+//        public int? Port { get; set; }
+//        public string Scheme { get; set; }
+//        public string StoreName { get; set; }
+//        public string StoreLocation { get; set; }
+//        public string FilePath { get; set; }
+//        public string Password { get; set; }
+//    }
+//}
