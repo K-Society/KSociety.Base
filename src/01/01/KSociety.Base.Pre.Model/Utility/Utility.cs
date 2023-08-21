44cfad4b6151b7686312c4be7e0a3faef20fef50
maniglia <kSociety85@gmail.com>
2023-08-21 19:27:23 +0200

    Added copyright on all files.

--- src/01/01/KSociety.Base.Pre.Model/Utility/Utility.cs
+++ src/01/01/KSociety.Base.Pre.Model/Utility/Utility.cs
@@ -1,4 +1,6 @@
-﻿namespace KSociety.Base.Pre.Model.Utility
+﻿// Copyright © K-Society and contributors. All rights reserved. Licensed under the K-Society License. See LICENSE.TXT file in the project root for full license information.
+
+namespace KSociety.Base.Pre.Model.Utility
 {
     using System.Diagnostics;
     using System.Linq;
--- src/01/02/KSociety.Base.Srv.Contract/Control/IDatabaseControlAsync.cs
+++ src/01/02/KSociety.Base.Srv.Contract/Control/IDatabaseControlAsync.cs
@@ -1,4 +1,6 @@
-﻿namespace KSociety.Base.Srv.Contract.Control
+﻿// Copyright © K-Society and contributors. All rights reserved. Licensed under the K-Society License. See LICENSE.TXT file in the project root for full license information.
+
+namespace KSociety.Base.Srv.Contract.Control
 {
     using KSociety.Base.App.Utility.Dto.Res.Control;
     using System.Threading.Tasks;
