4e8fcc3553e45652345f45e5a7c9773b4d71cdca
maniglia <a.klede@hotmail.it>
2025-05-29 14:07:19 +0200

    Nuget update.

--- src/01/KSocietyBaseEventBus/KSociety.Base.EventBus/ExchangeType.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright © K-Society and contributors. All rights reserved. Licensed under the K-Society License. See LICENSE.TXT file in the project root for full license information.
-
-namespace KSociety.Base.EventBus
-{
-    public enum ExchangeType { Direct = 1, Fanout = 2, Topic = 3, Headers = 4 }
-}
--- src/01/KSocietyBaseEventBus/KSociety.Base.EventBus/SubscriptionManagerType.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright © K-Society and contributors. All rights reserved. Licensed under the K-Society License. See LICENSE.TXT file in the project root for full license information.
-
-namespace KSociety.Base.EventBus
-{
-    public enum SubscriptionManagerType { Dynamic = 1, Typed = 2, Queue = 3, Rpc = 4, RpcClient = 5, RpcServer = 6 }
-}
